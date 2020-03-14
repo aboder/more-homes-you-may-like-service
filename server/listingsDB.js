6c4733d5666e2db376d7de74c3b98a821d9e3410
Chris Tucker <christuckeraudio@gmail.com>
2020-03-13 20:38:23 -0700

    Added db model and test file with 2 basic tests.

---
+++ server/listingsDB.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+mongoose.promise = require('bluebird');
+
+mongoose.connect('mongodb://localhost/listingFetcher');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+  console.log('connected to the db');
+});
+
+
+// create listingSchema
+// reminder: mongoose adds a '_id' property to every document (type: ObjectId);
+const listingSchema = new mongoose.Schema({
+  images: [String],
+  name: String,
+  title: String,
+  size: String,
+  description: String,
+  price: Number,
+  reviewScore: Number,
+  reviewCount: Number,
+});
+
+
+// create Listing model
+const Listing = mongoose.model('Listing', listingSchema);
+
+// save method for a single listing
+const save = (data) => {
+  const newListing = new Listing({
+    images: data.images,
+    name: data.name,
+    title: data.title,
+    size: data.size,
+    description: data.description,
+    price: data.price,
+    reviewScore: data.reviewScore,
+    reviewCount: data.reviewCount,
+  });
+
+  console.log('about to save this newListing to the db: ', JSON.stringify(newListing));
+
+  const p = newListing.save().then((result) => {
+    console.log('saved result: ', result);
+  }).catch((err) => {
+    console.log('there was an error: ', err);
+  });
+  return p;
+};
+
+// method to fetch all listings
+const getAllListings = () => Listing.find();
+
+
+module.exports = {
+  save,
+  getAllListings,
+};
